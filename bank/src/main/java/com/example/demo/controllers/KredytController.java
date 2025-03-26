cc59864c3f64fdf3dae332dc6cb01012cbc730f1
Samuel Ujazda <samuelujazda@gmail.com>
2025-03-26 13:37:51 +0100

    working with JSON format and postman, still no lombok

--- bank/src/main/java/com/example/demo/controllers/KredytController.java
+++ bank/src/main/java/com/example/demo/controllers/KredytController.java
@@ -3,19 +3,36 @@ package com.example.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.KredytModel;
 import com.example.demo.services.KredytService;
 
-@Controller
+@RestController
 @RequestMapping(path="/kredyt")
 public class KredytController {
-	@Autowired private KredytService kredytService;
-	@GetMapping public @ResponseBody double Calculate(
-			@RequestParam double amount) {
-		return kredytService.Calculate(amount, 0.1, 12);
+	
+    @Autowired
+    private KredytService kredytService;
+    
+    @Autowired
+    private KredytModel kredytModel;
+	
+	@PostMapping("/kredyt3")
+	public KredytModel getKredyt3(
+	@RequestBody KredytModel kredyt) {
+	System.out.println(
+	kredyt.getAmount()+"," +kredyt.getPercent()+","
+	+kredyt.getInstallmentAmount()+"," + Math.round(kredyt.getInstallment()*100) / 100.0);
+	
+	double rata = kredytService.Calculate(kredyt.getAmount(), kredyt.getPercent(), kredyt.getInstallmentAmount());
+    kredyt.setInstallment(rata);
+    
+	return kredyt;
 	}
 }
--- bank/src/main/java/com/example/demo/services/KredytService.java
+++ bank/src/main/java/com/example/demo/services/KredytService.java
@@ -6,7 +6,7 @@ import com.example.demo.models.KredytModel;
 
 @Service
 public class KredytService {
-	public double Calculate(double amount, double percent, double installmentAmount) {
+	public double Calculate(double amount, double percent, int installmentAmount) {
 		double m = 1 - 1 / Math.pow(1.0 + percent / 12, installmentAmount);
 		double installment = amount * (percent / 12) / m;
 		return installment;
