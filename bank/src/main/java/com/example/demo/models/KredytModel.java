f1b12ec7d6e852f2ca584fc7963cadc47d5f405e
Samuel Ujazda <samuelujazda@gmail.com>
2025-03-28 18:43:17 +0100

    finished with loan installement calculator usuign lombok and other spring tools, plus postman

--- bank/src/main/java/com/example/demo/models/KredytModel.java
+++ bank/src/main/java/com/example/demo/models/KredytModel.java
@@ -15,25 +15,4 @@ public class KredytModel {
 	private double percent;
 	private int installmentAmount;
 	private double installment;
-	
-//	public double Calculate(double amount, double percent, int installmentAmount) {
-//		this.amount = amount;
-//		this.percent = percent;
-//		this.installmentAmount = installmentAmount;
-//		double m = 1 - 1 / Math.pow(1.0 + percent / 12, installmentAmount);
-//		this.installment = amount * (percent / 12) / m;
-//		return installment;
-//	}
-	
-	public double getAmount() { return amount; }
-    public void setAmount(double amount) { this.amount = amount; }
-
-    public double getPercent() { return percent; }
-    public void setPercent(double percent) { this.percent = percent; }
-
-    public int getInstallmentAmount() { return installmentAmount; }
-    public void setInstallmentAmount(int installmentAmount) { this.installmentAmount = installmentAmount; }
-    
-    public double getInstallment() { return installment; }
-    public void setInstallment(double installment) { this.installment = installment; }
 }
